beb4c9d754c5e8ecd32e6eb7d6612636e6698240
JoelKueh <61089485+JoelKueh@users.noreply.github.com>
2020-05-10 15:51:06 -0500

    Certain buttons run code now.

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,25 +1,28 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class CalculatorGUI extends JFrame {
     private JPanel mainPanel;
     private JButton a0Button;
-    private JButton button2;
-    private JButton button3;
+    private JButton decimalButton;
+    private JButton calculateButton;
     private JButton a1Button;
     private JButton a4Button;
     private JButton a7Button;
     private JButton a8Button;
-    private JButton button8;
-    private JButton button9;
-    private JButton xButton;
-    private JButton button11;
+    private JButton addButton;
+    private JButton divideButton;
+    private JButton multiplyButton;
+    private JButton subtractButton;
     private JButton a3Button;
     private JButton a2Button;
     private JButton a5Button;
     private JButton a6Button;
     private JButton a9Button;
-    private JButton CEButton;
-    private JButton button1;
+    private JButton clearButton;
+    private JButton exponentButton;
     private JButton rootButton;
     private JButton piButton;
     private JTextArea inputStringTextArea;
@@ -31,11 +34,76 @@ public class CalculatorGUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+
+
+        decimalButton.addActionListener(new BtnClicked(decimalButton.getText()));
+        calculateButton.addActionListener(new BtnClicked(calculateButton.getText()));
+
+        a0Button.addActionListener(new NumberBtnClicked(a0Button.getText()));
+        a1Button.addActionListener(new NumberBtnClicked(a1Button.getText()));
+        a2Button.addActionListener(new NumberBtnClicked(a2Button.getText()));
+        a3Button.addActionListener(new NumberBtnClicked(a3Button.getText()));
+        a4Button.addActionListener(new NumberBtnClicked(a4Button.getText()));
+        a5Button.addActionListener(new NumberBtnClicked(a5Button.getText()));
+        a6Button.addActionListener(new NumberBtnClicked(a6Button.getText()));
+        a7Button.addActionListener(new NumberBtnClicked(a7Button.getText()));
+        a8Button.addActionListener(new NumberBtnClicked(a8Button.getText()));
+        a9Button.addActionListener(new NumberBtnClicked(a9Button.getText()));
+        piButton.addActionListener(new NumberBtnClicked(piButton.getText()));
+
+        addButton.addActionListener(new OperandBtnClicked(addButton.getText()));
+        divideButton.addActionListener(new OperandBtnClicked(divideButton.getText()));
+        multiplyButton.addActionListener(new OperandBtnClicked(multiplyButton.getText()));
+        subtractButton.addActionListener(new OperandBtnClicked(subtractButton.getText()));
+        exponentButton.addActionListener(new OperandBtnClicked(exponentButton.getText()));
+        rootButton.addActionListener(new OperandBtnClicked(rootButton.getText()));
+
+        clearButton.addActionListener(new BtnClicked(clearButton.getText()));
+    }
+
+    private class NumberBtnClicked implements ActionListener {
+        private String inputStr;
+
+        public NumberBtnClicked(String inputStr) {
+            this.inputStr = inputStr;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Number Clicked");
+
+
+        }
+    }
+
+    private class OperandBtnClicked implements ActionListener {
+        private String inputStr;
+
+        public OperandBtnClicked(String inputStr) {
+            this.inputStr = inputStr;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Operand Clicked");
+        }
+    }
+
+    private class ClearBtnClicked implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 
     public static void main(String[] args) {
         //Setting up GUI
         JFrame frame = new CalculatorGUI("My Calculator");
         frame.setVisible(true);
+
+        int sizeOfArray = 100;
+
+        int[] intArray = (int[])java.lang.reflect.Array.newInstance(int.class, sizeOfArray);
     }
 }
+
